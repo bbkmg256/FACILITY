38afc9005784fd34f8dd4a1f3e1462b8fe4fa7bb
BBKMG <132410342+bbkmg256@users.noreply.github.com>
2023-12-18 05:16:40 -0300

    Add files via upload

--- xfce4-install.sh
+++ xfce4-install.sh
@@ -5,6 +5,18 @@
 # NOTA: Basicamente un sistema minimo con xfce4 y algunos que otros complementos
 # PD de NOTA.: Ya dije que sistema minimo con xfce4 y alguna que otra cosa.
 
+# Chequea que las ejecuciones estén correctas...
+comprobar() {
+	if [ $(echo $?) == 0 ]; then
+		echo -e "[V] Todo correcto!\n"
+	else
+		echo -e "[ERROR] Algo falló...\n"
+		exit 1
+	fi
+	sleep 2
+}
+
+
 if [ $(whoami) == "root" ]; then
 	clear
 
@@ -16,57 +28,27 @@ if [ $(whoami) == "root" ]; then
 	echo -e "Modificando source.list...\n"
 	sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 	sudo echo -e "deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware\ndeb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware\ndeb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware\ndeb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware\ndeb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware\ndeb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware" > /etc/apt/sources.list
-	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
-	else
-		echo -e "[ERROR] Algo falló...\n"
-		exit 1
-	fi
-	sleep 2
+	comprobar
 
 	# Habilita paqueteria 32bits
 	echo -e "[!] Configurando arquitecturas 32 bits...\n"
 	sudo dpkg --add-architecture i386 &> /dev/null
-	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
-	else
-		echo -e "[ERROR] Algo falló...\n"
-		exit 1
-	fi
-	sleep 2
+	comprobar
 
 	# Actualizado de paqueteria
 	echo -e "[!] Actualizando lista de repositorios...\n"
 	sudo apt update
-	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
-	else
-		echo -e "[ERROR] Algo falló...\n"
-		exit 1
-	fi
-	sleep 2
+	comprobar
 
 	# Intalado de base xfce4
 	echo -e "[!] Instalando base de xfce4...\n"
 	sudo apt install -y xfdesktop4 xfwm4 xfce4-panel xfce4-settings xfce4-session thunar xfce4-power-manager xfce4-pulseaudio-plugin xfce4-notifyd
-	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
-	else
-		echo -e "[ERROR] Algo falló...\n"
-		exit 1
-	fi
-	sleep 2
+	comprobar
 
 	# Instalado de complementos utilies
 	echo -e "[!] Instalando complementos útiles...\n"
 	sudo apt install -y curl wget git zutty clamav ufw firefox-esr zsh linuxlogo network-manager network-manager-gnome pavucontrol pulseaudio neofetch gnome-themes-extra ntp htop rofi xarchiver
-	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
-	else
-		echo -e "[ERROR] Algo falló...\n"
-		exit 1
-	fi
-	sleep 2
+	comprobar
 
 	echo -e "[!] Instalación finalizada con exito, que tenga buen día :)\n"
 	sleep 2
