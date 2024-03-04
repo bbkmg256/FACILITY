a3f8037ab63f9bd36ed2dfde7009ab620a23dec6
BBKMG <132410342+bbkmg256@users.noreply.github.com>
2024-03-04 14:35:50 -0300

    Modificaciones menores
    NOTAS:
    
    - Colores agregados para mejor visualización
    - Cambio de banner

--- facility-script.sh
+++ facility-script.sh
@@ -3,13 +3,28 @@
 # Chequea que las ejecuciones estén correctas...
 comprobar() {
 	if [ $(echo $?) == 0 ]; then
-		echo -e "[V] Todo correcto!\n"
+		echo -e "$CORRECTO - Todo correcto!\n"
 	else
-		echo -e "[E] Algo falló...\n"
+		echo -e "$ERROR - Algo falló...\n"
 		exit 1
 	fi
 	sleep 2
 }
+
+
+# CONST
+# Colores
+NORMAL="[1;37m"
+VERDE="[1;32m"
+AMARILLO="[1;33m"
+ROJO="[1;31m"
+MAGENTA="[1;35m"
+
+# Carteles
+AVISO="\e$AMARILLO !\e$NORMAL"
+ERROR="\e$ROJO E\e$NORMAL"
+CORRECTO="\e$VERDE V\e$NORMAL"
+
 
 # INICIO
 if [ $(whoami) == "root" ]; then
@@ -18,81 +33,84 @@ if [ $(whoami) == "root" ]; then
 
  	# Banner...
  	echo -e "\n"
-	
-	echo -e "\n"
-	
- 	echo -e "- Bienvenido FACILITY-script...\n"
-	echo -e "- by bbkmg...\n"
+	echo -e "\e$VERDE"
+	echo -e '    dMMMMMP .aMMMb  .aMMMb  dMP dMP     dMP dMMMMMMP dMP dMP' 
+	echo -e '   dMP     dMP"dMP dMP"VMP amr dMP     amr    dMP   dMP.dMP '  
+	echo -e '  dMMMP   dMMMMMP dMP     dMP dMP     dMP    dMP    VMMMMP  '   
+	echo -e ' dMP     dMP dMP dMP.aMP dMP dMP     dMP    dMP   dA .dMP   '    
+	echo -e 'dMP     dMP dMP  VMMMP" dMP dMMMMMP dMP    dMP    VMMMP"    '     
+                                                             
+	echo -e "\e$NORMAL\n"
+ 	echo "- Bienvenido FACILITY-script..."
+	echo -e "- by\e$MAGENTA bbkmg\e$NORMAL...\n"
 	sleep 3
-
-	# Variable xd
-	val=1
 	
+
 	# Modificación de las lista de repos para aceptar paquete privativos
-	while [ $val -le 1 ]
+	while [ 1 -le 1 ]
 	do
-		read -p "[!] Desea modificar el fichero sources.list para paquetes privativos? (S / N): " opc
+		read -p " ! - Desea modificar el fichero sources.list para paquetes privativos? (S / N): " opc
 
 		if [[ $opc == "S" || $opc == "s" ]]; then
 			ls /etc/apt | grep sources.list.bak &> /dev/null
 			if [ $? == 1 ]; then
-				echo -e "[!] Creando backup... \n"
+				echo -e "$AVISO - Creando backup... \n"
 				sleep 1
 				sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-				echo -e "[!] Modificando sources.list...\n"
+				echo -e "$AVISO - Modificando sources.list...\n"
 				sleep 1
 				sudo cat ./src/deb-rep.txt &> /etc/apt/sources.list
 				comprobar
 			else
-				echo -e "[!] El fichero sources.list.bak ya existe, no se realizará ningun cambio...\n"
+				echo -e "$AVISO - El fichero sources.list.bak ya existe, no se realizará ningun cambio...\n"
 			fi
 			sleep 2
 			break
 
 		elif [[ $opc == "N" || $opc == "n" ]]; then
-			echo -e "[!] Modificación de ficheros sources.list omitida...\n"
+			echo -e "$AVISO - Modificación de ficheros sources.list omitida...\n"
 			sleep 2
 			break
 		else
-			echo -e "[E] Opción no válida...\n"
+			echo -e "$ERROR - Opción no válida...\n"
 			sleep 2
 		fi
 	done
 
 
 	# Habilitación de paqueteria 32bits
-	while [ $val -le 1 ]
+	while [ 1 -le 1 ]
 	do
-		read -p "[!] Habilitar paquetes 32bits? (S / N): " opc
+		read -p " ! - Habilitar paquetes 32bits? (S / N): " opc
 
 		if [[ $opc == "S" || $opc == "s" ]]; then
-			echo -e "[!] Configurando arquitecturas 32 bits...\n"
+			echo -e "$AVISO - Configurando arquitecturas 32 bits...\n"
 			sudo dpkg --add-architecture i386 &> /dev/null
 			comprobar
 			sleep 2
 			break
 
 		elif [[ $opc == "N" || $opc == "n" ]]; then
-			echo -e "[!] Omitido...\n"
+			echo -e "$AVISO - Omitido...\n"
 			sleep 2
 			break
 
 		else
-			echo -e "[E] Opción no válida...\n"
+			echo -e "$ERROR - Opción no válida...\n"
 			sleep 2
 		fi
 	done
 
 
 	# Actualización de paqueteria
-	echo -e "[!] Actualizando lista de repositorios...\n"
+	echo -e "$AVISO - Actualizando lista de repositorios...\n"
 	sudo apt update
 	comprobar
 	sleep 2
 
 
 	# Intalación de base xfce4
-	echo -e "[!] Instalando base de xfce4...\n"
+	echo -e "$AVISO - Instalando base de xfce4...\n"
 
 	sudo apt install -y $(cat ./src/xbase.txt | grep -v "#")
 	comprobar
@@ -100,12 +118,12 @@ if [ $(whoami) == "root" ]; then
 
 
 	# Instalacion de complementos utiles
-	while [ $val -le 1 ]
+	while [ 1 -le 1 ]
 	do
-		read -p "[!] Desea instalar algunos complementos útiles? (S / N): " opc
+		read -p " ! - Desea instalar algunos complementos útiles? (S / N): " opc
 
 		if [[ $opc == "S" || $opc == "s" ]]; then
-			echo -e "[!] Instalando complementos útiles...\n"
+			echo -e "$AVISO - Instalando complementos útiles...\n"
 
 			sudo apt install -y $(cat ./src/compl.txt | grep -v "#")
 			comprobar
@@ -113,23 +131,23 @@ if [ $(whoami) == "root" ]; then
 			break
 
 		elif [[ $opc == "N" || $opc == "n" ]]; then
-			echo -e "[!] Instalación de complementos omitida...\n"
+			echo -e "$AVISO - Instalación de complementos omitida...\n"
 			sleep 2
 			break
 
 		else
-			echo -e "[E] Opción no válida...\n"
+			echo -e "$ERROR - Opción no válida...\n"
 			sleep 2
 		fi
 	done
 
 
-	echo -e "[!] Instalación finalizada con exito, que tenga buen día :)\n"
+	echo -e "$CORRECTO - Instalación finalizada con exito, que tenga buen día :)\n"
 	sleep 2
 	exit 0
 
 else
-	echo -e "[E] Ejecute este script como super usuario porfavor.\n"
+	echo -e "$ERROR - Ejecute este script como super usuario porfavor.\n"
 	exit 1
 fi
 # FIN
